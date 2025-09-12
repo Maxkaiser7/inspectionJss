1711ad20a0d1e494c6a44408471214188e78f84e
Maxkaiser7 <maxkaiser950@gmail.com>
2025-09-12 09:13:07 +0200

    test

--- build-tablet.sh
+++ build-tablet.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-# Script pour builder l'APK tablette sans JAVA_HOME
+# Script pour builder l'APK tablette avec EAS sans JAVA_HOME local
+
+# 1️⃣ Aller dans le dossier du projet
+cd "$(dirname "$0")"
+
+# 2️⃣ Supprimer temporairement JAVA_HOME pour ce build
 env -u JAVA_HOME eas build --platform android --profile tablet-test
-chmod +x build-tablet.sh
